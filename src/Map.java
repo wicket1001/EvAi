7d9ddd24463c2ba22a67d985df03d5a9e2939b80
Harald Moritz <harald.moritz@htl.rennweg.at>
2017-10-25 17:41:16 +0200

    Working on Map.

--- src/Map.java
+++ src/Map.java
@@ -1,2 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Map {
+    private Field[][] fields;
+
+    Map(String filename) throws IOException {
+        List<String> fileContent = fileContent = Files.readAllLines(Paths.get(filename));
+        int max = 0;
+        for (String line: fileContent) {
+            max = Math.max(line.length(), max);
+        }
+        fields = new Field[fileContent.size()][max];
+
+        for (int i = 0; i < fileContent.size(); i++) {
+            String line = fileContent.get(i);
+            char[] charArray = line.toCharArray();
+            for (int j = 0; j < charArray.length; j++) {
+                char c = charArray[j];
+                fields[i][j] = new Field(Resource.toResource(c));
+            }
+        }
+    }
+
+
 }
--- src/Resource.java
+++ src/Resource.java
@@ -1,3 +1,13 @@
 public enum Resource {
-    food, water, wood, stone
+    food, water, wood, stone;
+
+    static Resource toResource(char c) {
+        switch (c) {
+            case 'n': return food;
+            case 'w': return water;
+            case 'h': return wood;
+            case 's': return stone;
+            default: throw new IllegalArgumentException("Illegal Field type: " + c);
+        }
+    }
 }
