7e96ad2eb56e373b71202e1e4c565ad48bf55afe
Harald Moritz <harald.moritz@htl.rennweg.at>
2017-10-25 18:01:33 +0200

    Added Javadoc to World. Added the first Map. Added a test case to Test.

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Resource r = Resource.food;
         System.out.println(r.name());
         Item i = Item.valueOf(r.name());
         System.out.println(i.name());
+
+        World w = new World("maps/map1.txt");
 
     }
 }
--- src/World.java
+++ src/World.java
@@ -4,9 +4,21 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
-public class World {
+/**
+ * The World, where the player plays.
+ */
+class World {
+    /**
+     * The different fields of the world.
+     */
     private Field[][] fields;
 
+    /**
+     * Creates a World from the contents of a file.
+     *
+     * @param filename The name of the file, from where the map generates.
+     * @throws IOException The IOException if there is something with the file.
+     */
     World(String filename) throws IOException {
         List<String> fileContent = fileContent = Files.readAllLines(Paths.get(filename));
         int max = 0;
@@ -22,14 +34,32 @@ public class World {
         }
     }
 
+    /**
+     * Gets a field at a specific coordinate.
+     *
+     * @param coordinate The coordinates, where to get the field from.
+     * @return The field at the coordinates.
+     */
     private Field getField(Coordinate coordinate) {
         return fields[coordinate.getX()][coordinate.getY()];
     }
 
+    /**
+     * Harvests a field at a specific coordinate.
+     *
+     * @param coordinate The coordinate of the field.
+     * @param tool Defines if a tool is used to harvest.
+     * @return The resource and the number harvested.
+     */
     Map<Resource, Integer> harvestField(Coordinate coordinate, boolean tool) {
         return getField(coordinate).harvest(tool);
     }
 
+    /**
+     * Regenerates the hole map ones.
+     *
+     * @return If every field is fully regenerated.
+     */
     boolean regenerate() {
         boolean regenerated = true;
         for (Field[] row: fields) {
