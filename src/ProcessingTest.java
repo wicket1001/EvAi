3041328bcc1990421eec0bb8fc49519ba5b54999
Lorenz Stechauner <lorenz.stechauner@htl.rennweg.at>
2017-10-25 19:45:57 +0200

    Processing

---
+++ src/ProcessingTest.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+public class ProcessingTest extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("ProcessingTest");
+    }
+
+    public void settings(){
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+
+}
