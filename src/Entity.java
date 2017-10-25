940f7555a3eade62566a308a9991b4f130aef5cc
Lorenz Stechauner <lorenz.stechauner@htl.rennweg.at>
2017-10-25 19:22:59 +0200

    Entity: move, getItemCount, set/getPos

--- src/Entity.java
+++ src/Entity.java
@@ -1,14 +1,37 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents an Entity and its AI
+ */
 public class Entity {
 
+    /**
+     * The Position of the Entity in the World
+     */
     private Coordinate pos;
+
+    /**
+     * The Inventory of the Entity
+     */
     private Map<Item, Integer> inventory;
+
+    /**
+     * Is the Entity alive?
+     */
     private boolean alive;
+
+    /**
+     * A 2D Node-Array to save the Nodes of the Neural Network
+     */
     private Node[][] nodes;
 
 
+    /**
+     * Constructor for a Entity
+     * @param pos The Position of the Entity in the World
+     * @param nodeNums The Number of Nodes per Layer
+     */
     public Entity( Coordinate pos, int[] nodeNums ) {
         this.pos = pos;
         alive = true;
@@ -38,18 +61,69 @@ public class Entity {
         }
     }
 
+    /**
+     * Getter for Position
+     * @return The Position of the Entity
+     */
+    public Coordinate getPos() {
+        return pos;
+    }
+
+    /**
+     * Setter for Position
+     * @param pos The new Position of the Entity
+     */
+    public void setPos( Coordinate pos ) {
+        this.pos = pos;
+    }
+
+    /**
+     * Moves the Entity relative to its Position
+     * @param vec Relative Movement
+     */
+    public void move( Coordinate vec ) {
+        this.pos.add( vec );
+    }
+
+    /**
+     * Is the Entity alive?
+     * @return is alive
+     */
     public boolean isAlive() {
         return alive;
     }
 
+    /**
+     * Is the Entity dead?
+     * @return is dead
+     */
     public boolean isDead() {
         return !alive;
     }
 
+    /**
+     * Adds amount to a certain item in the Inventory
+     * @param item The Item Stack to be modified
+     * @param amount The amount the Stack should be changed
+     */
     public void addToInventory( Item item, int amount ) {
         inventory.put( item, inventory.get(item) + amount );
     }
 
+    /**
+     * Returns the amount of the item in the Inventory
+     * @param item The amount of this item
+     * @return The amount
+     */
+    public int getItemCount( Item item ) {
+        return inventory.get( item );
+    }
+
+    /**
+     * Performs an AI Step and returns the Output-node-Values
+     * @param fields neighbour Fields
+     * @return Ouput Node Values
+     */
     public double[] step( double[] fields ) {
         nodes[0][0].setValue( (double) inventory.get(Item.food) / 100 );
         nodes[0][1].setValue( (double) inventory.get(Item.water) / 100 );
